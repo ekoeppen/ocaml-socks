aed23143aed71845a8bf555732abda0fa6d42480
Your Name <you@example.com>
2017-04-12 19:56:03 +0200

    looks pretty fucked

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -5,9 +5,15 @@ open Topkg
 
 let () =
   let mirage = Conf.with_pkg ~default:false "mirage" in
-  Pkg.describe "socks" @@ fun c ->
+  let lwt = Conf.with_pkg ~default:true "lwt" in
+  let opams =
+    [ Pkg.opam_file "opam" ]
+  in
+  Pkg.describe ~opams "socks" @@ fun c ->
   let mirage = Conf.value c mirage in
+  let lwt = Conf.value c lwt in
   Ok [ Pkg.mllib "src/socks.mllib"
      ; Pkg.mllib ~cond:mirage "mirage/socks.mllib"
+     ; Pkg.mllib ~cond:lwt "src/socks_lwt.mllib"
      ; Pkg.test "test/test"
      ]
--- src/socks_lwt.ml
+++ src/socks_lwt.ml
@@ -1,25 +1,32 @@
-(* A SOCKS4a helper. Wraps a Lwt file_descr
+(* SOCKS helper. Wraps a Lwt file_descr *)
 
-*)
-
+open Rresult
 open Lwt
 open Socks
 open Socks_types
 
-let connect_client (username : string)
+let connect_socks4_client (username : string)
                    (proxy_fd_in   : Lwt_io.input_channel)
                    (proxy_fd_out  : Lwt_io.output_channel)
                     hostname port : bool Lwt.t =
-  let message = Socks.make_socks4_request ~username hostname port in
+  let rec read_request socks_header =
+    begin match Socks.parse_socks4_response socks_header with
+    | Error Incomplete_response ->
+      Lwt_io.read ~count:(1+1+2+4) proxy_fd_in >>= fun new_header ->
+      read_request (socks_header ^ new_header)
+    | x -> Lwt.return x
+    end
+  in
   try%lwt
-  Lwt_io.write proxy_fd_out message >>= fun () ->
-  Lwt_io.read ~count:(1+1+2+4) proxy_fd_in >>= fun result ->
-  (*TODO handle case when fewer than 8 bytes are read *)
-  if Result.Ok () = Socks.parse_response result
-  then return true
-  else return false
+    R.bind (Socks.make_socks4_request ~username ~hostname port)
+    (fun out_request ->
+    Lwt_io.write proxy_fd_out out_request >>= fun () ->
+    Lwt_io.read ~count:(1+1+2+4) proxy_fd_in >>= fun header ->
+    read_request header
+    ) >>= fun x -> R.reword_error (fun e -> Lwt.return e) x
   with
   | End_of_file -> return false
+
 
 let receive_request (client_fd_in : Lwt_io.input_channel) : Socks_types.request_result Lwt.t =
   (* read minimum amount of bytes needed*)
