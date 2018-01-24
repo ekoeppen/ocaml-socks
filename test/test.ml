62c3019bc99782555738c096fb01641c6e40f3da
Eckhart Koeppen <eck@40hz.org>
2018-01-24 22:12:31 +0200

    Remove Mirage part

--- test/test.ml
+++ test/test.ml
@@ -2,8 +2,6 @@ open OUnit2
 
 (** TODO: OUnit2 should detect test suites automatically. *)
 let all_suites = [
-  "OUnit tests" >::: TestSocks4.suite;
-  "SOCKS 4A QCheck tests" >::: Test_quickcheck_socks4.suite;
   "SOCKS 5  QCheck tests" >::: Test_socks5.suite;
   ]
 
