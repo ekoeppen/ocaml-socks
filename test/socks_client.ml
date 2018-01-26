2544040a7dae871eb6f7dae0546b2ab212a31c62
Eckhart Koeppen <eckhart.koppen@intel.com>
2018-01-26 14:09:18 +0200

    Connection test

--- test/socks_client.ml
+++ test/socks_client.ml
@@ -1,15 +1,30 @@
 open Socks
-open Socks_types
 open Lwt
 open Cmdliner
+open Rresult
+
+type t = {
+  oc: Lwt_io.output Lwt_io.channel;
+  ic: Lwt_io.input Lwt_io.channel;
+}
+
+let of_socket socket =
+  let oc = Lwt_io.of_fd ~mode:Lwt_io.output socket in
+  let ic = Lwt_io.of_fd ~mode:Lwt_io.input socket in
+  {oc; ic}
 
 let connect ~proxy ~host ~port =
+  let connect_str = R.get_ok (Socks.make_socks5_request (Connect {address = Domain_address host; port = port})) in
   let socket = Lwt_unix.socket Lwt_unix.PF_INET Lwt_unix.SOCK_STREAM 0 in
   let%lwt host_info = Lwt_unix.gethostbyname proxy in
   let server_address = host_info.Lwt_unix.h_addr_list.(0) in
   let%lwt () = Lwt_unix.connect socket (Lwt_unix.ADDR_INET (server_address, 1080)) in
   Logs.info (fun m -> m "Connected via %s to %s, port %d" proxy host port);
-  return (socket)
+  let conn = of_socket socket in
+  let%lwt () = Lwt_io.write conn.oc "\005\001\000" in
+  let%lwt response = Lwt_io.read ~count:2 conn.ic in
+  Logs.info (fun m -> m "Connection result: %d bytes" (String.length response));
+  return conn
 
 let client _ proxy host port =
   Lwt_main.run (connect ~proxy ~host ~port)
